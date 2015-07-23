f946351e68bf3385f04a8be7d0b01c5366426615
TimpleSoft <timplesoft@gmail.com>
2015-07-23 22:09:23 +0100

    Extra customización del admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,31 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from blogs.models import Post, Category
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('categories',)
+    search_fields = ('title', 'abstract')
+    list_display = ('title', 'abstract', 'imageUrl')
+
+    fieldsets = (
+
+        (None, {
+            'fields': ('owner', 'title', 'publication_date'),
+            'classes': ('wide',)
+        }),
+
+        ('Contenido', {
+            'fields': ('abstract', 'body', 'imageUrl'),
+            'classes': ('wide',)
+        }),
+
+        ('Extra', {
+           'fields': ('categories',),
+           'classes': ('wide', 'collapse')
+        }),
+
+    )
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
